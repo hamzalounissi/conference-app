623184332eb187a06d031b64c35edc06d0f187fd
hamzalounissi <hamzalounissi@gmail.com>
2020-04-18 21:54:54 +0100

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conferencedemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder<?> builder = DataSourceBuilder.create();
+		builder.url("jdbc:postgresql://localhost:5432/first-spring-app");
+		builder.password("123456");
+		builder.username("postgres");
+		System.out.println("My custom datasource bean has been initialized and set");
+		return builder.build();
+	}
+
+}
